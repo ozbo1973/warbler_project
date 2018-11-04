597f504ee5965e201fce4841a2a2686e7472f36e
Brady <bbbove20@gmail.com>
2018-11-03 18:30:41 -0600

    Styling navbar.

--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -8,6 +8,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import NavbarCss from "../styles/NavbarCss";
+import logoImg from "../images/warbler-logo.png";
 
 class Navbar extends Component {
   logout = e => {
@@ -18,35 +19,43 @@ class Navbar extends Component {
     let { classes, currentUser } = this.props;
     return (
       <div>
-        <AppBar className={classes.root} position="static">
+        <AppBar className={classes.nav_root} position="static">
           <Toolbar>
-            <Typography className={classes.nav_Brand} component={Link} to="/">
-              Warbler Logo Here
+            <Typography
+              align="center"
+              className={classes.nav_Brand}
+              component={Link}
+              to="/"
+            >
+              <img className={classes.logo} src={logoImg} />
             </Typography>
             {currentUser.isAuthenticated ? (
-              <div className="nav-buttons">
+              <div className={classes.nav_buttons}>
                 <Button
-                  className={classes.nav_btn}
+                  className={`${classes.nav_btn} ${classes.bg_inherit}`}
                   component={Link}
                   to={"/new_message"}
                 >
                   New Message
                 </Button>
-                <Button onClick={this.logout} className={classes.nav_btn}>
+                <Button
+                  onClick={this.logout}
+                  className={`${classes.nav_btn} ${classes.bg_inherit}`}
+                >
                   Log Out
                 </Button>
               </div>
             ) : (
-              <div className="nav-buttons">
+              <div className={classes.nav_buttons}>
                 <Button
-                  className={classes.nav_btn}
+                  className={`${classes.nav_btn} ${classes.bg_inherit}`}
                   component={Link}
                   to="/signup"
                 >
                   SignUp
                 </Button>{" "}
                 <Button
-                  className={classes.nav_btn}
+                  className={`${classes.nav_btn} ${classes.bg_inherit}`}
                   component={Link}
                   to="/signin"
                 >
--- warbler-client/src/styles/NavbarCss.js
+++ warbler-client/src/styles/NavbarCss.js
@@ -1,15 +1,30 @@
 //css styles for Navbar.
 import { main_btns } from "./MainCss";
+import bgImg from "../images/nav-bg.png";
+
 const NavbarCss = {
-  root: {
+  nav_root: {
     flexGrow: 1,
-    background: "#2196F3"
+    backgroundImage: `url(${bgImg})`,
+    backgroundPosition: "center center",
+    backgroundSize: "cover"
+    // background: "#2196F3"
   },
   nav_Brand: {
     flexGrow: 1,
     marginLeft: "2rem"
   },
-  nav_btn: main_btns
+  logo: {
+    height: "50px",
+    width: "50px"
+  },
+  nav_buttons: {
+    marginRight: "2rem"
+  },
+  nav_btn: main_btns,
+  bg_inherit: {
+    background: "inherit"
+  }
 };
 
 export default NavbarCss;
